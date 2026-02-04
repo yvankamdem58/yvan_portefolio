75d23d0a114bf0d0bb41c755e54cbac949315306
yvankamdem58 <kamdemyvan088@gmail.com>
2026-02-04 23:25:28 +0100

    Mise à jour homepage et base pour GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  base: '/', // 👈 nom EXACT de ton repo GitHub kami-portefolio
+  base: '/yvan_portefolio/', // 👈 nom EXACT de ton repo GitHub kami-portefolio
   plugins: [react()],
 })
